eaf477a7d6204059a66ef5767d344452bcfc7dfd
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-09-13 22:32:17 -0300

    feat: add zod req schema

---
+++ src/models/reqSchema.ts
@@ -0,0 +1,25 @@
+const { z } = require('zod')
+
+exports.userSchemaRequest = z
+    .object({
+        email: z
+            .string({
+                required_error: 'Email inválido',
+                invalid_type_error: 'Email inválido',
+            })
+            .email()
+            .trim(),
+        password: z
+            .string({
+                required_error: 'Senha inválida',
+                invalid_type_error: 'Senha inválida',
+            })
+            .trim(),
+        userName: z
+            .string({
+                required_error: 'Nome de usuário inválida',
+                invalid_type_error: 'Nome de usuário inválida',
+            })
+            .trim(),
+    })
+    .strict()
