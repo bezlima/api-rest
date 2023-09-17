0509a9fd5c1edd7c8f9a134144460c72fa394a44
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-09-16 21:09:15 -0300

    fix: verify email return

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -4,14 +4,15 @@ import { comparePassword } from '../utils/encrypt'
 
 const listUser = async (email: string, password: string) => {
     await databaseConnection()
-
-    const loginUser = await user.findOne({ email })
-
     let login
-    await comparePassword(password, loginUser.password).then((res: boolean) => {
-        return (login = res)
-    })
-
+    const loginUser = await user.findOne({ email })
+    if (loginUser) {
+        await comparePassword(password, loginUser.password).then((res: boolean) => {
+            return (login = res)
+        })
+    } else {
+        login = false
+    }
     return login
 }
 
