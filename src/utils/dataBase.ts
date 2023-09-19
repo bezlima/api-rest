e17b19852f8182e776cdc5bfbe46f062398714d9
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-09-19 17:22:06 -0300

    fix: db connection

--- src/utils/dataBase.ts
+++ src/utils/dataBase.ts
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
+import mongoose, { ConnectOptions } from 'mongoose'
 require('dotenv').config()
 
-const URl = process.env.MONGO_HOST
+const URl = process.env.MONGO_HOST!
 
 export const connectToDatabase = async () => {
     try {
         await mongoose.connect(URl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
+        } as ConnectOptions)
     } catch (error) {
         console.error('Erro ao conectar ao banco de dados:', error)
+        throw error
     }
 }
